e9fcfa04677fcce336414698603dc704b68d9ab6
Fausto Juarez <faustojuarezyelamos@gmail.com>
2024-01-23 20:41:37 -0300

    Full working project - 2

--- src/Display.tsx
+++ src/Display.tsx
@@ -1,37 +1,36 @@
-import { FaPause, FaPlay, FaUndo } from "react-icons/fa";
 import { DisplayState, formatTime } from "./helpers";
+import { FaPlay, FaPause, FaUndo } from "react-icons/fa";
 
 interface DisplayProps {
-    displayState: DisplayState;
-    reset: () => void;
-    startStop: (displayState: DisplayState) => void;
+  displayState: DisplayState;
+  reset: () => void;
+  startStop: (displayState: DisplayState) => void;
 }
 
 const Display: React.FC<DisplayProps> = ({
-    displayState,
-    reset,
-    startStop,
+  displayState,
+  reset,
+  startStop,
 }) => {
-    return(
-        <div className="display">
-            <h4 id="timer-label">{displayState.timeType}</h4>
-            <span 
-                id="time-left" 
-                style={{color: `${displayState.timerRunning ? "red" : "white"}`}}
-            >
-            {formatTime(displayState.time)}
-            </span>
-            <div>
-                <button id="start_stop" onClick={() => startStop(displayState)}>
-                    {displayState.timerRunning ? <FaPause/> : <FaPlay />}
-                </button>
-                <button id="reset">
-                    <FaUndo />
-                </button>
-            </div>
-        </div>
-        
-    )
-}
+  return (
+    <div className="display">
+      <h4 id="timer-label">{displayState.timeType}</h4>
+      <span
+        id="time-left"
+        style={{ color: `${displayState.timerRunning ? "red" : "white"}` }}
+      >
+        {formatTime(displayState.time)}
+      </span>
+      <div>
+        <button id="start_stop" onClick={() => startStop(displayState)}>
+          {displayState.timerRunning ? <FaPause /> : <FaPlay />}
+        </button>
+        <button id="reset" onClick={reset}>
+          <FaUndo />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Display
+export default Display;
